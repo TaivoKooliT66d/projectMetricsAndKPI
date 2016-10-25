2be72ac32793942993ef85c4317951b117c065d1
Taivo Liik <taivo.web@gmail.com>
2016-10-25 14:23:43 -0400

    Expored Spring Boot and planning to use this together with Gradle

---
+++ projectKPI/src/main/java/projectKPI/HelloController.java
@@ -0,0 +1,18 @@
+package projectKPI;
+
+/**
+ * Created by taivo on 25.10.16.
+ */
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
